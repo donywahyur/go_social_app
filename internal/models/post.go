891d80197bd8aa782651d20bb7e7fc7c37dee93b
donywahyur <donyramadhan3011@gmail.com>
2025-01-23 14:16:14 +0700

    update model comment struct

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,25 +1,29 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
 
 type Post struct {
-	ID        string    `json:"id"`
-	Content   string    `json:"content"`
-	Title     string    `json:"title"`
-	UserID    string    `json:"user_id"`
-	Tags      []string  `gorm:"type:json" json:"tags"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Version   int       `json:"version"`
-	Comments  []Comment `gorm:"foreignKey:PostID" json:"comments"`
-	User      User      `gorm:"foreignKey:UserID" json:"user"`
+	ID        string         `json:"id"`
+	Content   string         `json:"content"`
+	Title     string         `json:"title"`
+	UserID    string         `json:"user_id"`
+	Tags      pq.StringArray `gorm:"type:text[]" json:"tags"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	Version   int            `json:"version"`
+	Comments  []Comment      `gorm:"foreignKey:PostID" json:"comments"`
+	User      User           `gorm:"foreignKey:UserID" json:"user"`
 }
 
 type Comment struct {
-	ID        string `json:"id"`
-	PostID    string `json:"post_id"`
-	UserID    string `json:"user_id"`
-	Content   string `json:"content"`
-	CreatedAt string `json:"created_at"`
-	User      User   `gorm:"foreignKey:UserID" json:"user"`
+	ID        string    `json:"id"`
+	PostID    string    `json:"post_id"`
+	UserID    string    `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	User      User      `gorm:"foreignKey:UserID" json:"user"`
 }
