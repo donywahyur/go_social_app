c6bd80034b0a94761805d083c1159eb5106f1e0f
donywahyur <donyramadhan3011@gmail.com>
2025-01-23 10:47:57 +0700

    update db seed create user

--- internal/app/database.go
+++ internal/app/database.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"go_social_app/internal/env"
 	model "go_social_app/internal/models"
+	"go_social_app/internal/repositories"
 	"log"
+	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -18,7 +21,6 @@ func GetDB() *gorm.DB {
 	port := env.Get("DB_PORT", "5432")
 
 	dbAddress := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, name, port)
-	fmt.Println(dbAddress)
 	db, err := gorm.Open(postgres.Open(dbAddress), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -28,8 +30,56 @@ func GetDB() *gorm.DB {
 	if err != nil {
 		log.Fatal("Migration failed:", err)
 	}
-
-	log.Println("Migration succeeded")
+	seed(db)
 
 	return db
+}
+
+func seed(db *gorm.DB) {
+	roles := []model.Role{
+		{ID: "1", Name: "user", Description: "A user can create posts and comments", Level: 1},
+		{ID: "2", Name: "moderator", Description: "A moderator can update other users posts", Level: 2},
+		{ID: "3", Name: "admin", Description: "An admin can update and delete other users posts", Level: 3},
+	}
+
+	err := db.First(&model.Role{}).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			db.Create(&roles)
+		}
+	}
+
+	var usernames = []string{
+		"alice", "bob", "charlie", "dave", "eve", "frank", "grace", "heidi",
+		"ivan", "judy", "karl", "laura", "mallory", "nina", "oscar", "peggy",
+		"quinn", "rachel", "steve", "trent", "ursula", "victor", "wendy", "xander",
+		"yvonne", "zack", "amber", "brian", "carol", "doug", "eric", "fiona",
+		"george", "hannah", "ian", "jessica", "kevin", "lisa", "mike", "natalie",
+		"oliver", "peter", "queen", "ron", "susan", "tim", "uma", "vicky",
+		"walter", "xenia", "yasmin", "zoe",
+	}
+
+	err = db.First(&model.User{}).Error
+	userRepository := repositories.NewUserRepository(db)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			for _, username := range usernames {
+				hashedPassword, err := userRepository.HashPassword(username)
+				if err != nil {
+					panic(err)
+				}
+				user := model.User{
+					ID:        uuid.NewString(),
+					Username:  username,
+					Email:     username + "@example.com",
+					Password:  hashedPassword,
+					Role:      roles[0],
+					CreatedAt: fmt.Sprintf("%v", time.Now().Format(time.RFC3339)),
+					IsActive:  true,
+				}
+				db.Create(&user)
+			}
+		}
+	}
+
 }
--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -1,14 +1,92 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+	"gorm.io/gorm"
+)
 
 type User interface {
+	HashPassword(password string) (string, error)
+	CompareHash(password, passwordHash string) (bool, error)
 }
 
 type repository struct {
-	db *gorm.DB
+	db    *gorm.DB
+	param params
+}
+
+type params struct {
+	memory      uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
 }
 
 func NewUserRepository(db *gorm.DB) *repository {
-	return &repository{db}
+	return &repository{
+		db,
+		params{
+			memory:      64 * 1024,
+			iterations:  3,
+			parallelism: 2,
+			saltLength:  16,
+			keyLength:   32,
+		}}
+}
+
+func (r *repository) HashPassword(password string) (string, error) {
+	salt := make([]byte, r.param.saltLength)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, r.param.iterations, r.param.memory, r.param.parallelism, r.param.keyLength)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	encodedHash := fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, r.param.memory, r.param.iterations, r.param.parallelism, b64Salt, b64Hash)
+
+	return encodedHash, nil
+}
+
+func (r *repository) CompareHash(password, passwordHash string) (bool, error) {
+	vals := strings.Split(passwordHash, "$")
+	if len(vals) != 6 {
+		return false, errors.New("invalid hash")
+	}
+
+	var memory, time uint32
+	var parallelism uint8
+
+	_, err := fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &memory, &time, &parallelism)
+	if err != nil {
+		return false, err
+	}
+
+	salt, err := base64.RawStdEncoding.DecodeString(vals[4])
+	if err != nil {
+		return false, err
+	}
+
+	decryptedHash, err := base64.RawStdEncoding.Strict().DecodeString(vals[5])
+	if err != nil {
+		return false, err
+	}
+
+	var keyLen = uint32(len(decryptedHash))
+
+	comparisonHash := argon2.IDKey([]byte(password), salt, time, memory, parallelism, keyLen)
+
+	return subtle.ConstantTimeCompare(comparisonHash, decryptedHash) == 1, nil
+
 }
